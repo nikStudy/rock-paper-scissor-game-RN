39da8d05769e6510612d0e01eed54819a35fd31d
nikStudy <nikhil.mittal60@gmail.com>
2020-11-24 17:50:05 +0530

    enabled web service workers

---
+++ web-build/precache-manifest.227b56a86bea12cfcf3777c57f360ee0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "58d30d3e7c57b3b5a6da",
+    "url": "/static/js/app.d4ad5d52.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "fc51335313dd7ab1f1bf",
+    "url": "/static/js/2.b9510d40.chunk.js"
+  },
+  {
+    "revision": "6c2a82f7c36a9279422113556107a710",
+    "url": "/static/media/rock.337d558c.png"
+  },
+  {
+    "revision": "375c7c6a134aa7055de646d47c2c9eec",
+    "url": "/static/media/paper.d4a03a15.png"
+  },
+  {
+    "revision": "293adbc7267032bbda26eff114d049b2",
+    "url": "/static/media/scissors.004f1986.png"
+  },
+  {
+    "revision": "345777057d8fd41a04235200cd2b5b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b9510d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbaee8e89e98936e63c68babcd869175",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e4bbeb6837ffa5df0c64a7aab9564162",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "b36390337375110dc819d64cc827c80c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "09a219279fbeda4763e007a6e0d7002f",
+    "url": "/manifest.json"
+  }
+];
